f74ddccfc57cc55cdd0a2d142c0071e40db72e50
kurata321 <kurata.hironori321@gmail.com>
2023-08-27 23:13:39 +0900

    add hash link comment to bold underline

--- apps/nch/src/app/threads/[dat]/Thread.tsx
+++ apps/nch/src/app/threads/[dat]/Thread.tsx
@@ -1,6 +1,8 @@
 import { headers } from 'next/headers'
+import Link from 'next/link'
 import fetch from 'node-fetch'
 import { decode } from 'html-entities'
+import { ThreadTitle } from './ThreadTitle'
 
 export const runtime = 'edge'
 
@@ -14,21 +16,20 @@ export const fetchThread = async (dat: string) => {
   return data.split('\n').map((sread) => sread.split('<>')).filter(x => x.length !== 1)
 }
 
-// リンクを置き換える関数
 const replaceReferences = (text: string) => {
   const decodedText = decode(text)
     .replace(/<br>/g, '\n')
     .replace(/&gt;/g, '>')
     .replace(/&lt;/g, '<')
 
-  // 正規表現でマッチング
+  // >> 3 のような形式の参照をリンクにする
   const parts = decodedText.split(/(>>\s?\d+)/g)
 
   return parts.map((part, index) => {
     const match = part.match(/>>\s?(\d+)/)
     if (match) {
       const id = match[1]
-      return <a key={index} href={`#${id}`} className="text-blue-600 dark:text-blue-400">{part}</a>
+      return <Link key={index} href={`#${id}`} className="text-blue-600 dark:text-blue-400">{part}</Link>
     } else {
       return part
     }
@@ -42,9 +43,9 @@ export const Thread = async ({ dat }: { dat: string }) => {
     <>
       {thread?.map((x, i) => (
         <div id={String(i + 1)} key={x[2]} className={'mb-2'}>
-          <div className={'text-sm dark:text-slate-300 light:text-slate-500'}>
+          <ThreadTitle id={i + 1}>
             {i + 1}. <span className={'font-bold'}>{decode(x[0])}</span> {x[2]}
-          </div>
+          </ThreadTitle>
           <div className={'whitespace-pre-wrap break-words'}>
             {replaceReferences(x[3])}
           </div>
